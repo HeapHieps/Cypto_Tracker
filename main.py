299a33ede7374b15abe0f3f2dfc7b4b25da375b7
HeapHieps <117422956+HeapHieps@users.noreply.github.com>
2023-06-08 23:22:07 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests 
+import tkinter as tk
+from datetime import datetime
+
+def Tracker():
+    url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    response = requests.get(url).json()
+    price = response["USD"]
+    Updatedtime = datetime.now().strftime("%H:%M:%S")
+
+    labelPrice.config(text = str(price)+ " $")
+    labelTime.config(text = "Updated at: " + Updatedtime)
+
+    canvas.after(5000, Tracker) #Update Prices every 5 seconds
+
+canvas = tk.Tk()
+canvas.geometry ("400x500")
+canvas.title ("Tracker")
+
+f1 = ("poppins", 22, "bold")
+f2 = ("poppins", 20, "bold")
+f3 = ("poppins", 16, "normal")
+
+label = tk.Label(canvas, text="BitCoin Price", font=f1)
+label.pack(anchor='w', padx=10, pady=10) 
+
+labelPrice = tk.Label(canvas, font=f2)
+labelPrice.pack(anchor='w', padx=10) 
+
+labelTime = tk.Label(canvas, font=f3)
+labelTime.pack(anchor='center', pady=50)
+
+Tracker()
+
+canvas.mainloop()
+
